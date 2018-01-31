853b25efcc4cd54b404028d64c1ea7477df20406
czbirka <christophe.zbirka@wanadoo.fr>
2018-01-31 13:36:40 +0100

    mise en place menu + option raz

--- MMM_TP1/app/src/main/java/com/christophe/mmm_tp1/MainActivity.java
+++ MMM_TP1/app/src/main/java/com/christophe/mmm_tp1/MainActivity.java
@@ -1,11 +1,18 @@
 package com.christophe.mmm_tp1;
 
+import android.os.Build;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.text.InputType;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +45,66 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.raz:
+                edt_nom.setText("");
+                edt_prenom.setText("");
+                edt_date.setText("");
+                edt_ville.setText("");
+                break;
+            case R.id.telephone:
+//                EditText tel = new EditText(this);
+//                tel.setInputType(InputType.TYPE_CLASS_PHONE);
+//                tel.setText("456");
+//                tel.setEms(10);
+//
+//                ConstraintLayout.LayoutParams lp = (ConstraintLayout.LayoutParams) tel.getLayoutParams();
+//                lp.height=10;
+//                tel.setLayoutParams(lp);
+//
+//                ConstraintLayout.LayoutParams mCparams = new ConstraintLayout.LayoutParams(
+//                        ConstraintLayout.LayoutParams.WRAP_CONTENT,
+//                        ConstraintLayout.LayoutParams.WRAP_CONTENT
+//                );
+//                tel.setLayoutParams(mCparams);
+//
+//                ViewGroup viewGroup = (ViewGroup) findViewById(R.id.myLayout);
+//                viewGroup.addView (tel);
+//
+//                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+//                    tel.setId(Utils.generateViewId());
+//                } else {
+//                    tel.setId(View.generateViewId());
+//                }
+//
+//                layout = (ConstraintLayout) findViewById(R.id.myLayout);
+//                mConstraintSet.clone(layout);
+//
+//                mConstraintSet.addToVerticalChain(tel.getId(),R.id.editText3,R.id.spinner2);
+//                mConstraintSet.centerHorizontally(tel.getId(), R.id.myLayout);
+//
+//
+//
+//
+//                layout.setConstraintSet(mConstraintSet);
+//
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 
     private boolean verificationSaisie(){
